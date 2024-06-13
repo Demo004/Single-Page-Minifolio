28b05622644c3042f780f498b82c3629cd754052
Ademola Taiwo <taiwoademola465@gmaile.com>
2024-06-13 21:59:52 +0100

    completed the projects and contacts sections

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import "../style/components/header.css";
 import "../style/components/hero.css";
 import "../style/components/stack.css";
 import "../style/components/projects.css";
+import "../style/components/contact.css";
 
 // utility
 import "../style/utility.css";
