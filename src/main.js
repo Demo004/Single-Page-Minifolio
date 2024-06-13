93b7469eeaa67c5bdca61d8c620401e2afcaa78a
Ademola Taiwo <taiwoademola465@gmaile.com>
2024-06-13 18:23:50 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// modern-normalize
+import "../style/modern-normalize.css";
+
+// style
+import "../style/style.css";
+
+// components
+import "../style/components/header.css";
+import "../style/components/hero.css";
+import "../style/components/stack.css";
+import "../style/components/projects.css";
+
+// utility
+import "../style/utility.css";
